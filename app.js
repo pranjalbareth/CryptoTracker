95ec2376865cd75bbe4a2843d0da6558cab806ae
Pranjal Bareth <pranjalnirmala@gmail.com>
2023-10-13 14:31:21 +0530

    Added : Target Coin Feature 💵

--- app.js
+++ app.js
@@ -2,30 +2,27 @@ const form = document.querySelector('#searchForm');
 const res = document.querySelector('#tableResult');
 var upd;
 form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (upd) {
+    clearTimeout(upd);
+  }
 
-    e.preventDefault();
-    if (upd) {
-        clearTimeout(upd);
-    }
+  const coinType = form.elements.coinType.value;
+  const targetCurrency = form.elements.targetCurrency.value;
 
-    const ctype = form.elements.coinType.value;
-
-    fetchPrice(ctype);
-
+  fetchPrice(coinType, targetCurrency);
 });
 
-
-const fetchPrice = async (ctype) => {
-    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
-    console.log(r.data.coin.price);
-    const price = r.data.coin.price;
-    const volume = r.data.coin.volume;
-    const change = r.data.coin.priceChange1d;
-    const base = r.data.coin.name;
-    const target = 'USD';
+const fetchPrice = async (coinType, targetCurrency) => {
+  const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${coinType}?currency=${targetCurrency}`);
+  const coinData = r.data.coin;
+  const price = coinData.price;
+  const volume = coinData.volume;
+  const change = coinData.priceChange1d;
+  const base = coinData.name;
 
 
-    res.innerHTML = `
+  res.innerHTML = `
     <thead>
         <tr>
             <th style="    background: #0d6efd;
@@ -39,7 +36,7 @@ const fetchPrice = async (ctype) => {
               <th scope="row" style="    background: #3e4c5c;
               color:#b3ebff;">${base}</th>
               <td style="    background: #5d6b7d;
-              color: #b3ebff;">${price} ${target}</td>
+              color: #b3ebff;">${price} <b>${targetCurrency}</b></td>
             </tr>
             <tr>
               <th scope="row" style="background: #3e4c5c;
@@ -55,5 +52,5 @@ const fetchPrice = async (ctype) => {
             </tr>
           </tbody>
      `
-    upd = setTimeout(() => fetchPrice(ctype), 10000);
+  upd = setTimeout(() => fetchPrice(ctype), 10000);
 }
